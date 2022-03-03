f35dc3f80a5fe80acf2c9917d3dd2ddf6fc6d237
tmdgus2568 <tmdgus2568@naver.com>
2022-03-03 23:43:33 +0900

    second commit

--- GitCloneTest/GitCloneTest/Program.cs
+++ GitCloneTest/GitCloneTest/Program.cs
@@ -2,9 +2,9 @@
 
 namespace GitCloneTest
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
