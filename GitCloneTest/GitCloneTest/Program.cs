9d537b70b4b64c37c2f39cbf6263c99c375743fc
tmdgus2568 <tmdgus2568@naver.com>
2022-03-03 23:23:14 +0900

    git clone test

---
+++ GitCloneTest/GitCloneTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitCloneTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
