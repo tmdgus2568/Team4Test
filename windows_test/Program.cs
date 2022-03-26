badcea1d9d50cef34263e03fcfb35e09568828ca
Hyunu97 <at6jean3@gmail.com>
2022-03-27 00:09:05 +0900

    12:08

--- windows_test/Program.cs
+++ windows_test/Program.cs
@@ -14,7 +14,7 @@ namespace windows_test
 
             Console.WriteLine("Test from 이대익 + 2022-03-26, 11:43 2222222222222222222222222222222222222222222222222");
 
-            Console.WriteLine("Test from 안현욱");
+            Console.WriteLine("Test from 안현욱 2022-03-26 12:08");
 
             Console.WriteLine("Test from 최혁준");
 
